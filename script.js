dbf5a25f2213e293cf62e7e63813c9f79ca8418e
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-05-13 14:32:18 +0700

    finish exercise

---
+++ script.js
@@ -0,0 +1,14 @@
+const products = {
+	product_id: SF0001,
+	product_name: "Sofa",
+	quantity_in_stock: 999,
+	unit_price: 599,
+	product_description: {
+		product_brand: "Elviro",
+		size_or_quantity: "XL",
+		taste_or_scent: null,
+		color: "Cream",
+		additional_description:
+			"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+};
